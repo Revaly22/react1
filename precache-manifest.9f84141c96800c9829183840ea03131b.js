ba17a15eff31697605a4f61e45024ff9d0dfcfe4
unknown <mosesrevaly@gmail.com>
2020-08-12 04:33:13 -0700

    Updates

---
+++ precache-manifest.9f84141c96800c9829183840ea03131b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f67c671c7327b8b53dcfc6a2969ed8c",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "59f6764d0fdc34e4e5fa",
+    "url": "/react1/static/css/main.ba874137.chunk.css"
+  },
+  {
+    "revision": "d72361c5ad48539ef9fc",
+    "url": "/react1/static/js/2.d008ba88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react1/static/js/2.d008ba88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f6764d0fdc34e4e5fa",
+    "url": "/react1/static/js/main.f145e577.chunk.js"
+  },
+  {
+    "revision": "c7e1b4ce0bd8bbf0238e",
+    "url": "/react1/static/js/runtime-main.c171fb9a.js"
+  }
+]);
